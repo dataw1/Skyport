175d3fc0e5d2175748ea23f8fb1a02702baf9847
dataw1 <grzegorz.twa@wp.pl>
2026-04-08 20:17:30 +0200

    Dodanie rejestracji

--- main.py
+++ main.py
@@ -67,64 +67,106 @@ def utworz_admina_przy_starcie():
 @app.get("/", response_class=HTMLResponse)
 async def strona_glowna(request: Request):
     user = get_current_user(request)
-    return templates.TemplateResponse(request=request, name="index.html", context={"request": request, "user": None})
+    return templates.TemplateResponse(request=request, name="index.html", context={"request": request, "user": user})
 
 @app.get("/loty", response_class=HTMLResponse)
 async def strona_loty(request: Request):
     user = get_current_user(request)
-    return templates.TemplateResponse(request=request, name="loty.html", context={"request": request, "user": None})
+    return templates.TemplateResponse(request=request, name="loty.html", context={"request": request, "user": user})
 
 @app.get("/parking", response_class=HTMLResponse)
 async def strona_parking(request: Request):
     user = get_current_user(request)
-    return templates.TemplateResponse(request=request, name="parking.html", context={"request": request, "user": None})
+    return templates.TemplateResponse(request=request, name="parking.html", context={"request": request, "user": user})
 
 @app.get("/mapy", response_class=HTMLResponse)
 async def strona_mapy(request: Request):
     user = get_current_user(request)
-    return templates.TemplateResponse(request=request, name="mapy.html", context={"request": request, "user": None})
+    return templates.TemplateResponse(request=request, name="mapy.html", context={"request": request, "user": user})
 
 @app.get("/bezpieczenstwo", response_class=HTMLResponse)
 async def strona_bezpieczenstwo(request: Request):
     user = get_current_user(request)
-    return templates.TemplateResponse(request=request, name="bezpieczenstwo.html", context={"request": request, "user": None})
+    return templates.TemplateResponse(request=request, name="bezpieczenstwo.html", context={"request": request, "user": user})
 
 @app.get("/pomoc", response_class=HTMLResponse)
 async def strona_pomoc(request: Request):
     user = get_current_user(request)
-    return templates.TemplateResponse(request=request, name="pomoc.html", context={"request": request, "user": None})
+    return templates.TemplateResponse(request=request, name="pomoc.html", context={"request": request, "user": user})
 
 @app.get("/logowanie", response_class=HTMLResponse)
 async def strona_logowania(request: Request):
     user = get_current_user(request)
-    return templates.TemplateResponse(request=request, name="logowanie.html", context={"request": request, "user": None})
+    return templates.TemplateResponse(request=request, name="logowanie.html", context={"request": request, "user": user})
 
 
+
+@app.post("/auth/register")
+async def rejestracja_post(imie: str = Form(...), nazwisko: str = Form(...), email: str = Form(...), haslo: str = Form(...)):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    try:
+        haslo_hash = pwd_context.hash(haslo)
+        
+
+        cur.execute(
+            "INSERT INTO Konta (email, haslo, rola) VALUES (%s, %s, %s) RETURNING id_konta",
+            (email, haslo_hash, 'pasazer')
+        )
+        nowe_id_konta = cur.fetchone()[0]
+
+
+        cur.execute(
+            "INSERT INTO Pasazerowie (id_konta, imie, nazwisko) VALUES (%s, %s, %s)",
+            (nowe_id_konta, imie, nazwisko)
+        )
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(f"Błąd rejestracji: {e}")
+        return RedirectResponse(url="/logowanie?error=register_failed", status_code=status.HTTP_302_FOUND)
+    finally:
+        cur.close()
+        conn.close()
+
+
+    return RedirectResponse(url="/logowanie?success=registered", status_code=status.HTTP_302_FOUND)
 
 @app.post("/auth/login")
 async def logowanie_post(email: str = Form(...), haslo: str = Form(...)):
-    # Pobranie danych uzyt
     conn = get_db_connection()
     cur = conn.cursor(cursor_factory=RealDictCursor)
     cur.execute("SELECT * FROM Konta WHERE email = %s", (email,))
     user_db = cur.fetchone()
-    cur.close()
-    conn.close()
 
-    # Sprawdzanie hasla
     if user_db and pwd_context.verify(haslo, user_db['haslo']):
+
+        imie_uzytkownika = "Admin" 
+        
+        if user_db['rola'] == 'pasazer':
+            cur.execute("SELECT imie FROM Pasazerowie WHERE id_konta = %s", (user_db['id_konta'],))
+            pasazer = cur.fetchone()
+            if pasazer:
+                imie_uzytkownika = pasazer['imie']
+
 
         token = jwt.encode({
             "email": user_db['email'],
             "rola": user_db['rola'],
             "id": user_db['id_konta'],
+            "imie": imie_uzytkownika, 
             "exp": datetime.utcnow() + timedelta(hours=8)
         }, SECRET_KEY, algorithm=ALGORITHM)
+
+        cur.close()
+        conn.close()
 
         response = RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
         response.set_cookie(key="session_token", value=token, httponly=True)
         return response
     
+    cur.close()
+    conn.close()
     return RedirectResponse(url="/logowanie?error=invalid", status_code=status.HTTP_302_FOUND)
 
 @app.get("/wyloguj")
